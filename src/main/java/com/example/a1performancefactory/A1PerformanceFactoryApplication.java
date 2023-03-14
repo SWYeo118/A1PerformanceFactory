d30a2893d62bfe34cc652e01f8e43e6ba4ef1f21
여상우 <effortmeta@gmail.com>
2023-03-14 22:25:13 +0900

    secondCommit

---
+++ src/main/java/com/example/a1performancefactory/A1PerformanceFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.a1performancefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A1PerformanceFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A1PerformanceFactoryApplication.class, args);
+	}
+
+}
